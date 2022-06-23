67442b0acf70642c54a60951914d8ba40f18c5dc
Lokulee <104442618+Lokulee@users.noreply.github.com>
2022-06-23 19:47:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,24 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.2.3"
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.19.0"
      }
     }
    }
 
 provider "aws" {
-        region = "us-east-1" 
-        #profile = "default"
-	}
+	region = "us-east-1"
+    access_key = "AKIAVHYY5Y3H3TJXS55N"
+    secret_key = "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"
+	# version = "~> 3.37"
+}
 
-resource "aws_instance" "Raminfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
+resource "aws_instance" "Lokesh" {
+	ami = "ami-0cff7528ff583bf9a"
 	instance_type = "t2.micro"
+	tags = {
+        	Name = "Terraform-Server"
+ 	}
 }
